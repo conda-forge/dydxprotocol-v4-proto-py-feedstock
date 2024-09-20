b3d9e8343ad28f301daab2a6c784291f4ff2d44d
memento <claude.rc@gmail.com>
2024-09-20 16:04:18 -0500

    (fix) wrong docker syntax

--- recipe/helpers/proto-builder.ps1
+++ recipe/helpers/proto-builder.ps1
@@ -1,19 +1,32 @@
-@"
+function Set-Dockerfile {
+    param (
+        [string]$FilePath
+    )
+    @"
 FROM mcr.microsoft.com/windows/servercore:ltsc2022
 RUN powershell -Command `
-    Set-ExecutionPolicy Bypass -Scope Process -Force; `
+    "Set-ExecutionPolicy Bypass -Scope Process -Force; `
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; `
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); `
     choco install protobuf -y; `
-    choco install buf -y
+    choco install buf -y"
 WORKDIR /workspace
-"@ | Out-File -FilePath $env:SRC_DIR/Dockerfile -Encoding utf8
+"@ | Out-File -FilePath $FilePath -Encoding utf8
+}
 
-# Inspect the Dockerfile
-Get-Content -Path $env:SRC_DIR/Dockerfile -Raw
-# Build the Docker image
+function Update-Makefile {
+    param (
+        [string]$MakefilePath
+    )
+
+    $makefileContent = Get-Content -Path $MakefilePath -Raw
+    $updatedContent = $makefileContent -replace 'protoImageName=ghcr.io/cosmos/proto-builder:\$\(protoVer\)', 'protoImageName=local/proto-builder:latest'
+    $updatedContent | Out-File -FilePath $MakefilePath -Encoding utf8
+}
+
+Set-Dockerfile -FilePath "$env:SRC_DIR/Dockerfile"
+Get-Content -Path "$env:SRC_DIR/Dockerfile" -Raw
+
 docker build -t local/proto-builder:latest $env:SRC_DIR
 
-$makefileContent = Get-Content -Path Makefile -Raw
-$updatedContent = $makefileContent -replace 'protoImageName=ghcr.io/cosmos/proto-builder:\$\(protoVer\)', 'protoImageName=local/proto-builder:latest'
-$updatedContent | Out-File -FilePath Makefile -Encoding utf8
+Update-Makefile -MakefilePath "$env:SRC_DIR/Makefile"
